03a369ae9bc19707e086be3c8a6475ab7f417a28
Vansladd <taiwoajoms@gmail.com>
2024-09-05 16:54:25 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import "./Home.css"
 import restaurantImage from "./restaurant.jpg"
-const contentDiv = document.querySelector("#content");
+import menu from "./Menu"
+import about from "./About"
 
+
+const homeBtn = document.querySelector("#Home");
+const menuBtn = document.querySelector("#Menu");
+const aboutBtn = document.querySelector("#About");
+const contentDiv = document.querySelector("#content");
 
 const image = document.createElement("img");
 
@@ -12,5 +18,22 @@ const header = document.createElement("h1");
 header.innerText = "Vansladd's Restaurant";
 
 contentDiv.appendChild(header)
-
 contentDiv.appendChild(image)
+
+menuBtn.addEventListener("click",() =>{
+    contentDiv.innerHTML="";
+    menu(contentDiv);
+})
+
+homeBtn.addEventListener("click",() => {
+    contentDiv.innerHTML = "";
+    contentDiv.appendChild(header)
+    contentDiv.appendChild(image)
+})
+
+aboutBtn.addEventListener("click", () => {
+    contentDiv.innerHTML = "";
+    about(contentDiv);
+})
+
+
